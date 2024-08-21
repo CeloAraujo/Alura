de43972b972d193918468524c6b302a62d7de671
marcelo.araujo <marcelo.araujo@memora.com.br>
2024-08-21 18:40:26 -0300

    paginação e busca nos favoritos

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -11,7 +11,11 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+  listar(
+    pagina: number,
+    filtro: string,
+    favoritos: boolean
+  ): Observable<Pensamento[]> {
     const itensPorPagina = 6;
 
     let params = new HttpParams()
@@ -20,6 +24,9 @@ export class PensamentoService {
 
     if (filtro.trim().length > 2) {
       params = params.set('q', filtro);
+    }
+    if (favoritos) {
+      params = params.set('favorito', true);
     }
     return this.http.get<Pensamento[]>(this.API, { params });
   }
@@ -33,10 +40,10 @@ export class PensamentoService {
     return this.http.put<Pensamento>(url, pensamento);
   }
 
-  mudarFavorito(pensamento:Pensamento):Observable<Pensamento>{
-    pensamento.favorito=!pensamento.favorito
-    const url =  `${this.API}/${pensamento.id}`;
-    return this.http.put<Pensamento>(url,pensamento);
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito;
+    const url = `${this.API}/${pensamento.id}`;
+    return this.http.put<Pensamento>(url, pensamento);
   }
 
   excluir(id: number): Observable<Pensamento> {
